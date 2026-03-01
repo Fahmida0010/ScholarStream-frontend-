b29eb9c424a706f25f8b5520a4ddb6e8931c9eca
Fahmida0010 <fahmidaakter0010@gmail.com>
2026-03-02 01:48:08 +0600

    main jsx pge updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import { RouterProvider } from "react-router/dom";
-// import { router } from './routes/Routes.jsx';
-// import AuthProvider from './contexts/AuthProvider.jsx';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { Toaster } from 'react-hot-toast';
-// const queryClient = new QueryClient();
-
-
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <QueryClientProvider client={queryClient}> 
-//      <AuthProvider>
-//       <RouterProvider router={router} />
-//       <Toaster position='top-right' reverseOrder={false} />
-//      </AuthProvider>
-//       {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-//       </QueryClientProvider>
-//   </StrictMode>,
-// )
- 
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
